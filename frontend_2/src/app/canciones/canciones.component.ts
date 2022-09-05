5fc589419722345ee6cbdc0de1d4a5766e15e7e4
jsanchezl12 <js.sanchezl12@uniandes.edu.co>
2022-09-05 18:01:12 -0500

    frontend2

---
+++ frontend_2/src/app/canciones/canciones.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Cancion } from './cancion';
+import { CancionesService } from './canciones.service';
+
+@Component({
+  selector: 'app-canciones',
+  templateUrl: './canciones.component.html',
+  styleUrls: ['./canciones.component.css']
+})
+export class CancionesComponent implements OnInit {
+
+  constructor(private cancionesService: CancionesService) { }
+  canciones: Array<Cancion>;
+
+  getCancionesList() {
+    this.cancionesService.getCanciones('').subscribe(cs => {
+      this.canciones = cs;
+    });
+  }
+  ngOnInit() {
+    this.getCancionesList();
+  }
+
+}
